2ca713ac2462b465acc4b867f92428af6213016f
smilepyt <307080368@qq.com>
2019-05-26 20:27:02 +0800

    UI组件

--- src/router.js
+++ src/router.js
@@ -8,7 +8,12 @@ import Buttons from "./pages/ui/buttons";
 import Modals from "./pages/ui/modals";
 import Loading from "./pages/ui/loading";
 import Notice from "./pages/ui/notice";
-import Messages from './pages/ui/messages'
+import Messages from "./pages/ui/messages";
+import Tabs from "./pages/ui/tabs";
+import Gallery from "./pages/ui/gallery";
+import Carousel from "./pages/ui/carousel";
+import FormLogin from "./pages/form/login";
+import FormRegister from "./pages/form/register";
 
 export default class router extends Component {
   render() {
@@ -22,12 +27,17 @@ export default class router extends Component {
               render={() => (
                 <Admin>
                   <Switch>
-                    <Route path="/admin/home" comment={Home} />
+                    <Route path="/admin/home" component={Home} />
                     <Route path="/admin/ui/buttons" component={Buttons} />
                     <Route path="/admin/ui/modals" component={Modals} />
                     <Route path="/admin/ui/loadings" component={Loading} />
                     <Route path="/admin/ui/notification" component={Notice} />
                     <Route path="/admin/ui/messages" component={Messages} />
+                    <Route path="/admin/ui/tabs" component={Tabs} />
+                    <Route path="/admin/ui/gallery" component={Gallery} />
+                    <Route path="/admin/ui/carousel" component={Carousel} />
+                    <Route path="/admin/form/login" component={FormLogin} />
+                    <Route path="/admin/form/reg" component={FormRegister} />
                   </Switch>
                 </Admin>
               )}
